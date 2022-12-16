25d8b5bc9175287fba78bd8c4b3be71429d14003
oumaro <c.oumaro@yahoo.com>
2022-12-16 18:41:23 +0100

    pages recherche , barre de recherche presque terminer

--- src/app/recherche/recherche.component.ts
+++ src/app/recherche/recherche.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MangasService } from '../Services/Mangas/mangas.service';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-recherche',
@@ -7,8 +8,13 @@ import { MangasService } from '../Services/Mangas/mangas.service';
   styleUrls: ['./recherche.component.scss']
 })
 export class RechercheComponent {
-public slides: any
-  constructor(private mangas: MangasService) {
+public slides: any;
+public search_mangas!: FormGroup;
+public listeManga = document.querySelector('.listeManga');
+public resultat: any[] = []; //la liste des mangas recuperer
+public booleen_result: Boolean = false;
+
+  constructor(private mangas: MangasService , private list_M: FormBuilder) {
 
   }
 
@@ -16,8 +22,44 @@ public slides: any
   ngOnInit() {
 
     this.slides = this.mangas.list_mangas;
-    console.log(this.slides);
 
+    this.search_mangas = this.list_M.group({
+
+      search: [],
+
+    })
+
+    console.log(this.search_mangas.value.search);
+
+  }
+
+
+mangas_value(){
+
+
+  console.log(this.search_mangas.value.search);
+
+  for(let mangas of  this.mangas.list_mangas){
+
+    if(mangas.name.toLowerCase() == this.search_mangas.value.search){
+
+      console.log(mangas);
+
+      this.resultat.push(mangas)
+      this.booleen_result = true;
+      console.log(this.resultat);
+
+
+    }else{
+            ///
+    }
+  }
+
+}
+
+  show_mangas(index: Number){
+
+    console.log(index);
 
   }
 }
